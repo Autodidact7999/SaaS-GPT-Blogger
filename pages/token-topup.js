95de95c6dca878481f002154921440112cfb58f4
Harsh Naik <harsh@Harshs-MacBook-Pro.local>
2023-08-25 18:14:22 -0700

    Added Protected Routs

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,6 +1,16 @@
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+
 export default function TokenTopUp() {
     return <div>
       <h1>Token</h1>
     </div>;
   }
   
+  export const getServerSideProps = withPageAuthRequired(() => {
+    return {
+        props: {
+            test:'this is a test',
+        },
+    };
+    });
+    
